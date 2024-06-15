ea4cbfd83f29104981d9d05d58dbd9ae9f2e32d0
Jogy Felix <jogyfelix1@gmail.com>
2024-06-16 00:14:44 +0530

    svg support added

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Welcome from './auth/Welcome'
 
 const index = () => {
   return (
-    <View>
-      <Text>WatchDog</Text>
-    </View>
+   <Welcome/>
   )
 }
 
